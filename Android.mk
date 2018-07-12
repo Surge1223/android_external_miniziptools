d789169990e68467ee63047244ffb33f96dd8f4a
Surge Raval <Surge1223@gmail.com>
2018-07-12 07:41:09 +0000

    fix up aosp compile

--- Android.mk
+++ Android.mk
@@ -1,32 +1,81 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := libminiunz
+LOCAL_C_INCLUDES := \
+    external/zlib \
+    external/zlib/src \
+    system/core/base/include \
+    system/core/include \
+    system/core/libziparchive \
+    system/core/liblog \
+    system/core/libcutils \
+    system/core/libcutils/include \
+    $(LOCAL_PATH)
 
 LOCAL_SRC_FILES := libminiunz.c ioapi.c
+LOCAL_CFLAGS := \
+  -DUSE_FILE32API -Wall \
+  -std=gnu99 -O2 -fpic -fPIC \
+  -pipe -fno-strict-aliasing \
+  -D_GNU_SOURCE  \
+  -D__BIONIC__ -DANDROID \
+  -D__ANDROID__ -DZLIB_CONST
 
-LOCAL_CFLAGS := -O2 -DUSE_FILE32API -Wall
-LOCAL_LDLIBS := -lz -llog
-
+LOCAL_STATIC_LIBRARIES :=  liblog libc libc++_static
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := minunz
+LOCAL_C_INCLUDES := \
+    external/zlib \
+    external/zlib/src \
+    system/core/base/include \
+    system/core/include \
+    system/core/libziparchive \
+    system/core/liblog \
+    system/core/libcutils \
+    system/core/libcutils/include \
+    $(LOCAL_PATH)
 
 LOCAL_SRC_FILES := miniunz.c unzip.c ioapi.c
-LOCAL_CFLAGS := -O2 -DUSE_FILE32API -Wall
-LOCAL_LDLIBS := -lz -llog
-LOCAL_LDFLAGS := -L/opt -static
+LOCAL_CFLAGS := \
+  -DUSE_FILE32API -Wall \
+  -std=gnu99 -O2 -fpic -fPIC \
+  -pipe -fno-strict-aliasing \
+  -D_GNU_SOURCE  \
+  -D__BIONIC__ -DANDROID \
+  -D__ANDROID__ -DZLIB_CONST
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libz liblog libminiunz libc libc++_static
+LOCAL_LDFLAGS :=  -static
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := minzip
+LOCAL_C_INCLUDES := \
+    external/zlib \
+    external/zlib/src \
+    system/core/base/include \
+    system/core/include \
+    system/core/libziparchive \
+    system/core/liblog \
+    system/core/libcutils \
+    system/core/libcutils/include \
+    $(LOCAL_PATH)
 
 LOCAL_SRC_FILES := minizip.c zip.c ioapi.c
+LOCAL_CFLAGS := \
+  -DUSE_FILE32API -Wall \
+  -std=gnu99 -O2 -fpic -fPIC \
+  -pipe -fno-strict-aliasing \
+  -D_GNU_SOURCE  \
+  -D__BIONIC__ -DANDROID \
+  -D__ANDROID__ -DZLIB_CONST
 
-LOCAL_CFLAGS := -O2 -DUSE_FILE32API -Wall
-LOCAL_LDLIBS := -lz -llog
-#LOCAL_WHOLE_STATIC_LIBRARIES := libminiunz
-LOCAL_LDFLAGS := -L/opt  -static
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libz liblog libminiunz libc libc++_static
+LOCAL_LDFLAGS :=  -static
 include $(BUILD_EXECUTABLE)
+
